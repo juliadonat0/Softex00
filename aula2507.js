2b83d4982bad6d74a56cbde522def8bee88db3dd
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-07-25 13:05:10 +0000

    SWITCH

--- aula2507.js
+++ aula2507.js
@@ -24,3 +24,97 @@ switch (diaSemana){
     default:
         console.log ("dia inválido")
 }
+
+
+var diaMes = 7
+switch (diaMes){
+    case 1: 
+        console.log ("Janeiro")
+       break
+    case 2: 
+        console.log ("Fevereiro")
+        break
+    case 3: 
+        console.log ("Março")
+        break
+    case 4: 
+        console.log ("Abril")
+        break
+    case 5: 
+        console.log ("Maio")
+        break
+    case 6: 
+        console.log ("Junho")
+        break
+    case 7: 
+        console.log ("Julho")
+        break
+        case 8: 
+        console.log ("Agosto")
+       break
+    case 9: 
+        console.log ("Setembro")
+        break
+    case 10: 
+        console.log ("Outubro")
+        break
+    case 11: 
+        console.log ("Novembro")
+        break
+    case 12: 
+        console.log ("Dezembro")
+        break
+    default:
+        console.log ("Mês inválido")
+}
+
+let estadoCivil = "solteiro"
+switch (estadoCivil){
+    case ("solteiro"): 
+        console.log ("Você é Solteiro!")
+       break
+    case ("casado"): 
+        console.log ("Você é Casado!")
+        break
+    case ("Divorciado"): 
+        console.log ("Você é Divorciado!")
+        break
+    default: 
+        console.log ("Outro")
+}
+
+let animal = ("cachorro")
+switch (animal){
+    case ("cachorro"): 
+        console.log ("Au-Au!")
+       break
+    case ("gato"): 
+        console.log ("Miau!")
+        break
+    case (passaro): 
+        console.log ("Piu!")
+        break
+    default: 
+        console.log ("Outro")
+}
+
+let num1 = 7
+let num2 = 9
+let operacao = ("subtração")
+
+switch (operacao){
+    case ("soma"): 
+        console.log (num1 + num2)
+       break
+    case ("subtração"): 
+        console.log (num1 - num2)
+        break
+    case ("divisão"): 
+        console.log (num1 / num2)
+        break
+    case ("multiplicação)"): 
+        console.log (num1 * num2)
+        break
+   default: 
+        console.log ("operação inválida")
+}
---
+++ exercicioclasse.js
@@ -0,0 +1,8 @@
+var idade = 18;
+if (idade < 16){
+    console.log ("Não pode dirigir")
+} else if (idade <= 16 || idade < 18){
+    console.log ("Pode dirigir com autorização")
+} else {
+    console.log("Pode dirigir")
+}
