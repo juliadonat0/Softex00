05c47aa5853c77ec609ee618c5026045d6f68edd
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-07-28 17:35:28 +0000

    exercício WHILE

--- while.js
+++ while.js
@@ -1,12 +1,24 @@
+// 1. imprima os número de 1 a 10
 let numero = 1
-
 
 while (numero <= 10){
     console.log (numero )
     numero++
  }
 
- var num =1 ;
+ //2. Calcule a soma dos número de 1 a 100 
+
+ let contador = 1
+ let soma = 0
+while (contador <= 100){
+     (soma += contador)
+        contador++
+}
+console.log(soma)
+
+// 3.Conte e imprima a quantidade de número pares de 1 a 50.
+ 
+var num =1 ;
  while (num <=50) {
      var resto = num%2;
      if (resto ==0){
@@ -14,3 +26,13 @@ while (numero <= 10){
      }
      num = num + 1;
  }
+
+ // 4 Multiplique um número por 2 até o resultado ser maior que 1000
+
+ var x = 250
+ var resultado = (x *2)
+ while (x * 2 <= 1000 ){
+    console.log
+
+ }
+ 
