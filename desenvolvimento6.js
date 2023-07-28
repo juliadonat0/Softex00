3bf844087bb3c7fdb861c3916682a8e1b06e552c
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-07-28 01:18:24 +0000

    JoyClass - Exercício Calculadora módulo 2.

---
+++ desenvolvimento6.js
@@ -0,0 +1,30 @@
+const readline = require("readline-sync");
+
+
+var num1 = Number(readline.question ("Digite o primeiro número"));
+var num2 = Number(readline.question ("Digite o segundo número"));
+var operacao = readline.question ("Qual a operação matematica");
+
+calculadora (num1,num2,operacao)
+
+function calculadora(num1,num2,operacao){
+  
+  switch (operacao){
+      case ("soma"): 
+          console.log ("A soma é: ", num1+num2)
+        break
+      case ("subtração"): 
+          console.log ("A subtração é: ", num1-num2)
+          break
+      case ("multiplicacao"): 
+          console.log ("A multiplicacao é: ", num1*num2)
+          break
+      case ("divisao"): 
+          console.log ("A divisao é: ", num1/num2)
+          break
+      default: 
+          console.log ("Operação não existe. O resultado é 0")
+  }
+}
+
+
