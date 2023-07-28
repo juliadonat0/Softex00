c45f3f449aee70e6ace2afc9b3b73cf07161ee52
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-07-28 15:24:44 +0000

    questão ficha - WHILE

---
+++ while.js
@@ -0,0 +1,16 @@
+let numero = 1
+
+
+while (numero <= 10){
+    console.log (numero )
+    numero++
+ }
+
+ var num =1 ;
+ while (num <=50) {
+     var resto = num%2;
+     if (resto ==0){
+         console.log("Par " + num);
+     }
+     num = num + 1;
+ }
