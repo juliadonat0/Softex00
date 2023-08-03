6344f49efd40bb936de16bfdf13931701f1f1ac6
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-03 00:49:09 +0000

    aula classe ARRAY

--- aulaarrays.js
+++ aulaarrays.js
@@ -36,24 +36,32 @@ var num3 = num1.concat(num2);
 console.log(num3)
 
 //7. Dado o array de números abaixo, crie um novo array contendo apenas os número pares.
-//exercicio7()
-//function exercicio7() {
-// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log("exercicio 7")
+const numeros1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numeros1.filter(x => x % 2 === 0));
 
-
-
-//}
 
 
 //8. Ordene o seguinte array de números em ordem crescente{
+console.log("exercicio 8")
 const numx = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 numx.sort();
 console.log(numx)
 
 //9. Dado o array de frutas abaixo, encontre o índice em que a fruta "laranja" está localizada.
-
+console.log("exercicio 9")
 const fruta = ["maça", "banana", "laranja", "uva", "manga"];
 console.log(fruta.indexOf("laranja"))
 
 //10. Crie uma função que receba um parâmetro e verifique se é um array ou não. 
 //A função deve retornar true se for um array e false caso contrário.
+console.log("exercicio 10")
+var doces = ["coco", "goiaba", "jaca"]
+function eUmArray(array) {
+    if (typeof array == 'arra') {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(` A variável doces é um array? ${eUmArray(10)}`);
