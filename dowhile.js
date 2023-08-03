ca8a0b45ec939a9a8e08a8769bdb00f203ca7159
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-03 14:18:00 +0000

    respondendo o restante das questões. Cont na 7.

--- dowhile.js
+++ dowhile.js
@@ -77,32 +77,38 @@ function exercicio2() {
         }
     }
 
- // 6. Faça o usuário digitar sua senha até que seja digitada a senha correta.
-exercicio6()
-function exercicio6() {
+    // 6. Faça o usuário digitar sua senha até que seja digitada a senha correta.
+    exercicio6()
+    function exercicio6() {
 
-    const readline = require("readline-sync");
-    var senhaUser = readline.question("Digite sua senha: ");
-    const SENHA = "C@sa"
+        const readline = require("readline-sync");
+        var senhaUser = readline.question("Digite sua senha: ");
+        const SENHA = "C@sa"
 
-    do {
-        if (senhaUser == SENHA) {
-            break
-        }
-        senhaUser = readline.question("Senha incorreta. Digite novamente!! ");
+        do {
+            if (senhaUser == SENHA) {
+                break
+            }
+            senhaUser = readline.question("Senha incorreta. Digite novamente!! ");
 
-    } while (senhaUser != SENHA)
-    console.log(`A senha ${senhaUser} está correta!!`)
+        } while (senhaUser != SENHA)
+        console.log(`A senha ${senhaUser} está correta!!`)
 
-}
+    }
+
     // 7. Imprima os múltiplos de 3 de 1 a 30.
-exercicio7()
-function exercicio7() {
-    let numero = 1
+    exercicio7()
+    function exercicio7() {
 
-    do {
-
-        console.log(numero * 3)
-        numero++
-    } while (numero <= 30)
+        let numero = 1
+        
+        do {
+            let resultado = (numero * 3)
+            console.log(resultado)
+            resultado++
+        }
+        while (numero >= 30)
+       
+    }
+    
 }
