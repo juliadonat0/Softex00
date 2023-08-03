f1635113fe5b8f8af6b74596a39880f04b0c7ad8
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-03 00:32:03 +0000

    verificação JoyClass

---
+++ testehoje.js
@@ -0,0 +1,21 @@
+//let numeros = [1, 2, 3, 4, 5]
+//console.log(numeros[1]);
+///let soma = console.log(numeros[4] + numeros[3])
+
+//let tamanhoFixo = Array(3)
+//console.log(tamanhoFixo)
+
+//let numeros_1 = [1, 2, 3]
+//let maisNumeros = [...numeros_1, 4, 5, 6]
+//console.log(maisNumeros)
+
+//let numeros = [1, 2, 3, 4, 5];
+//for (let i = 0; i < numeros.length; i++) {
+  //  console.log(numeros[1])
+//}
+//console.log(numeros)
+
+let frutas = ['maça', 'banana', 'laranja'];
+frutas.forEach(function (fruta){
+    console.log(fruta);
+});
