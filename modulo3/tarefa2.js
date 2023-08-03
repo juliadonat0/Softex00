68b88d549a82f70ec527557eaa3a0c74e48a5ffe
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-03 00:32:46 +0000

    exercícios JoyClass

---
+++ modulo3/tarefa2.js
@@ -0,0 +1,30 @@
+//Nesta atividade, você deve criar um código em JavaScript com uma matriz bidimensional 
+//para armazenar informações sobre animais e as suas características.
+
+//Cada linha deve representar um animal e 
+//cada coluna terá as diferentes informações sobre ele, como nome, espécie e idade.
+
+
+let animal = Array();
+animal.push(['cão', 'dalmata', 3]);
+animal.push(['gato', 'domestico', 6]);
+animal.push(['pato', 'real', 9]);
+console.log(animal);
+
+
+
+
+//let nota = Array();
+//nota.push([5.5, 7.9, 7.1]);
+////nota.push([7, 3.5, 9]);
+//////nota.push([8.6, 7.3, 8.8]);
+
+//console.log(nota);
+
+//function calcularMedia(notasArray) {
+   // let soma = 0;
+   // for (let nota of notasArray) {
+   //     soma += nota;
+    //}
+   // console.log(nota / notasArray.lenght);
+//}
