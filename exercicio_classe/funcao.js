faaf0aec79465c8121e303104b10f351ea779d95
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-08 13:13:10 +0000

    aula 08.08.23

--- exercicio_classe/funcao.js
+++ exercicio_classe/funcao.js
@@ -1,5 +1,5 @@
-exibirSaudacao('Julia', 2023)
-function exibirSaudacao(nome, ano) {
-    console.log('Hello,', nome, ano)
+exibirSaudacao('Julia', 04, 'fevereiro')
+function exibirSaudacao(nome, dia, mes) {
+    console.log(`Hello, ${nome}. Sua data de aniversário é: ${dia} ${mes}.`)
 }
 
