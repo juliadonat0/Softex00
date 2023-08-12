4f5bdb1d3628a44e7a80b48ff55b26fb37277f60
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-12 16:25:04 +0000

    respostas exercício slide

--- exercicio_classe/funcao_com_retorno.js
+++ exercicio_classe/funcao_com_retorno.js
@@ -10,22 +10,91 @@ console.log(receberFuncao)
 
 //1. Crie uma função chamada somar que aceite dois números como parâmetros e retorne a soma deles.
 console.log("EXERCÍCIO 1")
-somar(30, 70)
+console.log(somar(30, 70))
+
 function somar(num1, num2) {
-    return somar = (num1 + num2)
+    return num1 + num2
 }
-let receberSoma = somar
-console.log(receberSoma)
 
 //2. Escreva uma função chamada ehPar que aceite um número com parâmetro e retorne true se o número
 //for par e false caso contrário.
 
-//CORRIGIR!!!!
 console.log("EXERCÍCIO 2")
-ehPar()
-function ehPar(numero = 7) {
-    numero % 2 == 0
+console.log(ehPar(8))
+
+function ehPar(numero) {
     return numero % 2 == 0
 }
-let resultado = ehPar
-console.log(resultado)
+
+//3. Crie uma função chamada maiorNumero que receba três números como parâmetros e retorne o maior deles.
+console.log("EXERCÍCIO 3")
+console.log(`O maior número é: `, maiorNumero(30, 58, 333))
+function maiorNumero(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1
+    } else if (num2 > num1 && num2 > num3) {
+        return num2
+    } else {
+        return num3
+    }
+
+}
+
+
+
+//4. Escreva uma função chamada calcularIMC que receba o peso e a altura de uma pessoa como parâmetros 
+//e retorne o índice de massa corporal (IMC) calculado.
+console.log("EXERCÍCIO 4")
+var resultadoIMC = calcularIMC(90, 1.81)
+console.log(`O resultado do IMC é: `, resultadoIMC, `E a classificação é: `, classificacaoIMC(resultadoIMC))
+
+function calcularIMC(peso, altura) {
+    return peso / Math.pow(altura, 2)
+}
+function classificacaoIMC(imc) {
+    if (imc < 18.5) {
+        return "MAGREZA"
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        return "NORMAL"
+    } else if (imc >= 25 && imc <= 29.9) {
+        return "SOBREPESO"
+    } else if (imc >= 30 && imc <= 39.9) {
+        return "OBESIDADE"
+    } else {
+        return "OBESIDADE GRAVE"
+    }
+}
+
+// outra opção de resolução
+
+console.log(calcularIMC2(190, 1.81))
+function calcularIMC2(peso, altura) {
+    var imc = peso / Math.pow(altura, 2)
+
+    if (imc < 18.5) {
+        return "O imc é " + imc + "isso é MAGREZA"
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        return "O imc é " + imc + "isso é NORMAL"
+    } else if (imc >= 25 && imc <= 29.9) {
+        return "O imc é " + imc + "isso é SOBREPESO"
+    } else if (imc >= 30 && imc <= 39.9) {
+        return "O imc é " + imc + "isso é OBESIDADE"
+    } else {
+        return `O imc é " ${imc}.  Isso é OBESIDADE GRAVE`
+    }
+}
+
+//5. Crie uma função chamada contarVogais que aceite uma string como parâmetro 
+//e retorne o número de vogais (a, e, i, o, u) presentes na string.
+contarvogais()
+function contarvogais(palavra){
+
+
+}
+
+//6. Escreva uma função chamada celsiusParaFahrenheit que receba uma temperatura em graus Celsius 
+//como parâmetro e retorne o valor equivalente em Fahrenheit.
+
+
+//7. Crie uma função chamada gerarSenha que aceite um parâmetro tamanho (número inteiro) e 
+//retorne uma senha aleatória com o tamanho especificado.
