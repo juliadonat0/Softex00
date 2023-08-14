0ae2d40667f479ad740852c74168973b23e78c81
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-14 21:23:52 +0000

    resolução final

--- modulo3/tarefa3.js
+++ modulo3/tarefa3.js
@@ -4,56 +4,35 @@
 //operações, qual seria a sequência dos números removidos em cada uma delas para esvaziá-las 
 //completamente utilizando o array mencionado?
 
-console.log("LISTA") 
-lista()
-function lista() {
-    let lista = [3, 7, 9, 1, 0];
-    lista.shift(0);
-    console.log(lista)
-    lista.shift(1);
-    console.log(lista)
-    lista.shift(2);
-    console.log(lista)
-    lista.shift(3);
-    console.log(lista)
-    lista.shift(4);
-    console.log(lista)
-}
-
-
-console.log("FILA")
+console.log("FILA") //Remova um elemento da frente de um array, usando o método shift(). Essa ação é o mesmo que a operação de desenfileiramento, ou seja, o primeiro é atendido e sai da fila.
 fila()
 function fila() {
     let fila = [3, 7, 9, 1, 0];
-       
-    fila.splice(4) 
-    console.log(fila)
-    fila.splice(3) 
-    console.log(fila)
-    fila.splice(4) 
-    console.log(fila)
-    fila.splice(3) 
-    console.log(fila)
-    fila.splice(2) 
-    console.log(fila)
-    fila.splice(1) 
-    console.log(fila)
-    fila.splice(0) 
-    console.log(fila)
-   }
+    for (contador = 0; contador <= fila.length + 1; contador++) {
+        fila.shift();
+        console.log(fila);
+    }
+}
 
-console.log("PILHA")
+console.log("LISTA")
+lista()
+function lista() {
+    let lista = [3, 7, 9, 1, 0];
+
+    for (contador = lista.length; contador >= 0; contador--) {
+        lista.splice(contador);
+        console.log(lista);
+    }
+}
+
+
+console.log("PILHA") //O método pop()remove o elemento final do array e retorna o elemento para os chamados. Se o array estiver vazio, o método retornará undefined.
 pilha()
-function pilha(){
-let pilha = [3, 7, 9, 1, 0];
-pilha.pop(0);
-console.log(pilha);
-pilha.pop(1);
-console.log(pilha);
-pilha.pop(2);
-console.log(pilha);
-pilha.pop(3);
-console.log(pilha);
-pilha.pop(4);
-console.log(pilha);
+function pilha() {
+    let pilha = [3, 7, 9, 1, 0];
+
+    for (let contador = 0; contador <= pilha.length + 1; contador++) {
+        pilha.pop()
+        console.log(pilha);
+    }
 }
