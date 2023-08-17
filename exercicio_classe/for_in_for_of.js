eaf98cba9c9ee5976aa278c2ccb73f32e7bd19b2
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-17 14:12:52 +0000

    próxima aula

---
+++ exercicio_classe/for_in_for_of.js
@@ -0,0 +1,17 @@
+console.log("EXERCÍCIO 1")
+const array = [3, 7, 2, 9, 5]
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
+
+console.log("EXERCÍCIO 2 = FOR..IN")
+const dados =[
+{a:10},
+{b:20},
+{c:30},
+{d:40},
+];
+
+for (let item in dados){
+    console.log()
+}
