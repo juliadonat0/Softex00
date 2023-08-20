9e7e67d45d225d19dc3d14c74037712477e5bae8
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-20 22:26:25 +0000

    resolução4  Joyclass

---
+++ javascript_e_orientacao_objeto/atividade4_cadastro.js
@@ -0,0 +1,12 @@
+var nome; // String
+var idade; // Number
+var salario; // Number
+var diploma; // Boolean
+
+cadastro("Xuxa", 60, 50000, false)
+function cadastro(nome, idade, salario, diploma) {
+    console.log(`Nome: ${nome}`);
+    console.log(`Idade: ${idade}`);
+    console.log(`Salário: ${salario}`);
+    console.log(`Diploma: ${diploma}`);
+}
