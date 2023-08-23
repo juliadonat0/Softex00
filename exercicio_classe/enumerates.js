717d735ee21dd69147aaa63222c5d995b2d56f01
juliadonat0 <139878632+juliadonat0@users.noreply.github.com>
2023-08-23 00:27:14 +0000

    questões

---
+++ exercicio_classe/enumerates.js
@@ -0,0 +1,9 @@
+// Suponha que você está desenvolvendo um aplicativo de agendamento e precisa representar os dias da semana. Como você usaria enums para isso e qual seria o benefício?
+
+// Você está construindo um jogo e precisa representar os diferentes tipos de inimigos. Como você usaria enums para essa situação?
+
+// Você está criando um aplicativo de reserva de passagens aéreas e precisa representar os diferentes tipos de classe (Econômica, Executiva, Primeira Classe). Como você usaria enums para isso?
+
+// Imagine que você está desenvolvendo um aplicativo de tarefas e deseja categorizar as tarefas em "Trabalho", "Estudo" e "Lazer". Como você usaria enums para esse cenário?
+
+// Em um aplicativo de e-commerce, você precisa definir os diferentes status de um pedido: "Em processamento", "Enviado" e "Entregue". Como você usaria enums para isso?
