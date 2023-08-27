f59fd67f3022b0acf848ae7e563e4bd02439cba0
Julia Donato <139878632+juliadonat0@users.noreply.github.com>
2023-08-27 20:38:52 -0300

    resolução joyclass

---
+++ javascript_e_orientacao_objeto/atividade3.js
@@ -0,0 +1,30 @@
+var nome   // String
+var idade //BigInt
+var salario //Symbol
+var diploma  // Boolean
+
+
+console.log(nome)
+console.log(idade)
+console.log(salario)
+console.log(diploma)
+
+nome = null
+idade = null
+salario = null
+diploma = null
+
+console.log(nome)
+console.log(idade)
+console.log(salario)
+console.log(diploma)
+
+nome = "Popeye"
+idade = 75
+salario = Symbol(1000)
+diploma = true
+
+console.log(nome)
+console.log(idade)
+console.log(salario)
+console.log(diploma)
