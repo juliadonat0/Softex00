f2e468664d06afddc443c328eb2ba644b6a82382
Julia Donato <139878632+juliadonat0@users.noreply.github.com>
2023-08-27 20:05:00 -0300

    resolução incompleta

--- exercicio_classe/enumerates.js
+++ exercicio_classe/enumerates.js
@@ -1,15 +1,15 @@
 // 1. Suponha que você está desenvolvendo um aplicativo de agendamento e precisa representar
 // os dias da semana. Como você usaria enums para isso e qual seria o benefício?
 
-console.log ("Exercício 1")
+console.log("Exercício 1")
 const diasDaSemana = {
-DOMINGO: 'Domingo',
-SEGUNDA: 'Segunda-feira',
-TERCA:'Terça-feira',
-QUARTA: 'Quarta-feira',
-QUINTA: 'Quinta-feira',
-SEXTA: 'Sexta-feira',
-SÁBADO: 'Sábado'
+    DOMINGO: 'Domingo',
+    SEGUNDA: 'Segunda-feira',
+    TERCA: 'Terça-feira',
+    QUARTA: 'Quarta-feira',
+    QUINTA: 'Quinta-feira',
+    SEXTA: 'Sexta-feira',
+    SÁBADO: 'Sábado'
 }
 
 console.log(diasDaSemana.QUARTA)
@@ -17,11 +17,11 @@ console.log(diasDaSemana.QUARTA)
 // 2. Você está construindo um jogo e precisa representar os diferentes tipos de inimigos. 
 //Como você usaria enums para essa situação?
 
-console.log ("Exercício 2")
+console.log("Exercício 2")
 const inimigos = {
-INICIAL: 'XXX',
-INTERMEDIARIO: 'YYY',
-SUPERIOR:'ZZZ',
+    INICIAL: 'XXX',
+    INTERMEDIARIO: 'YYY',
+    SUPERIOR: 'ZZZ',
 }
 
 console.log(inimigos.INICIAL)
@@ -30,11 +30,11 @@ console.log(inimigos.INICIAL)
 // 3. Você está criando um aplicativo de reserva de passagens aéreas e precisa representar 
 //os diferentes tipos de classe (Econômica, Executiva, Primeira Classe). Como você usaria enums para isso?
 
-console.log ("Exercício 3")
+console.log("Exercício 3")
 const classe = {
-ECONOMICA: 'Econômica',
-EXECUTIVA: 'Executiva',
-PRIMEIRACLASSE:'Primeira Classe',
+    ECONOMICA: 'Econômica',
+    EXECUTIVA: 'Executiva',
+    PRIMEIRACLASSE: 'Primeira Classe',
 }
 
 console.log(classe.EXECUTIVA)
@@ -42,14 +42,23 @@ console.log(classe.EXECUTIVA)
 // 4. Imagine que você está desenvolvendo um aplicativo de tarefas e deseja categorizar as 
 //tarefas em "Trabalho", "Estudo" e "Lazer". Como você usaria enums para esse cenário?
 
-console.log ("Exercício 3")
+console.log("Exercício 4")
 const tarefa = {
-TRABALHO: ' Trabalho',
-ESTUDO: 'Estudo',
-LAZER:'Primeira Lazer',
+    TRABALHO: ' Trabalho',
+    ESTUDO: 'Estudo',
+    LAZER: 'Primeira Lazer',
 }
 
-console.log(classe.ESTUDO)
+console.log(tarefa.ESTUDO)
 
 
-// Em um aplicativo de e-commerce, você precisa definir os diferentes status de um pedido: "Em processamento", "Enviado" e "Entregue". Como você usaria enums para isso?
+// 5. Em um aplicativo de e-commerce, você precisa definir os diferentes 
+//status de um pedido: "Em processamento", "Enviado" e "Entregue". 
+//Como você usaria enums para isso?
+console.log("Exercício 5")
+const pedido = {
+    STATUS1: 'Em Processamento',
+    STATUS2: 'Enviado',
+    STATUS3: 'Entregue'
+}
+console.log(pedido.STATUS2)
