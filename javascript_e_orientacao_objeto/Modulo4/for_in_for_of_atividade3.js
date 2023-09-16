6bf2b7eef2cae167eeed91db93016f37eb618cb8
Julia Donato <139878632+juliadonat0@users.noreply.github.com>
2023-09-16 01:38:33 +0000

    Resolução do JoyClass

--- javascript_e_orientacao_objeto/Modulo4/for_in_for_of_atividade3.js
+++ javascript_e_orientacao_objeto/Modulo4/for_in_for_of_atividade3.js
@@ -1,22 +1,18 @@
-const pessoa1 = [
-    {
-        Nome: "Alice",
-        Idade: 18,
-        Profissao: "enfermeira",
-        Cidade: "Recife"
-    }
-];
+class Pessoa {
+    constructor(nome, idade, profissao, cidade) {
+      this.nome = nome;
+      this.idade = idade;
+      this.profissao = profissao;
+      this.cidade = cidade
+    }  
+  }
 
-const pessoa2 = [
-    {
-        Nome: "Bruno",
-        Idade: 47,
-        Profissao: "medico",
-        Cidade: "Jaboatão"
-    }
-];
+const pessoa1 = new Pessoa("Alice", 18, "enfermeira", "Recife")
+const pessoa2 = new Pessoa("Bruno", 47, "medico", "Jaboatão")
+const pessoa3 = new Pessoa("Maria", 78, "baba", "Fortaleza")
+const pessoa4 = new Pessoa("Manoel", 36, "gari", "Salvador")
 
-const pessoas = [pessoa1, pessoa2];
+const pessoas = [pessoa1, pessoa2, pessoa3, pessoa4];
 
 console.log (`FOR..IN`)
 
@@ -26,6 +22,10 @@ for (let i in pessoas) {
 
 console.log (`FOR..OF`)
 for (let p of pessoas) {
-    console.log(`Nome: ${pessoa1}`)
-   
+    console.log(`
+    Nome: ${p.nome}
+    Idade: ${p.idade}
+    Profissão: ${p.profissao}
+    Cidade: ${p.cidade}
+    `)
 }
