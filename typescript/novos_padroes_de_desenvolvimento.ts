0e1cb0353163fba98eb0456fa899d17b7795f9e8
Julia Donato <139878632+juliadonat0@users.noreply.github.com>
2023-10-07 18:18:18 +0000

    exercício salad e aula - slide 2 completo

--- typescript/novos_padroes_de_desenvolvimento.ts
+++ typescript/novos_padroes_de_desenvolvimento.ts
@@ -1,3 +1,5 @@
+import { arrayBuffer } from "stream/consumers"
+
 //1. Crie uma classe Pessoa com os atributos nome, idade e email.
 class Pessoa {
     nome: string
@@ -75,40 +77,200 @@ console.log(circulo.areaCirculo())
 console.log(circulo.circunferencia())
 
 // 5. Crie uma classe ContaBancaria com os atributos saldo e numeroConta e métodos para depositar, sacar e verificar o saldo.
-class ContaBancaria {
-    numeroConta: number
-    saldo: number
+// class ContaBancaria {
+//     numeroConta: number
+//     saldo: number
 
-    constructor(numeroConta: number, saldo: number) {
-        this.numeroConta = numeroConta
-        this.saldo = saldo
+//     constructor(numeroConta: number, saldo: number) {
+//         this.numeroConta = numeroConta
+//         this.saldo = saldo
+//     }
+
+//     depositar(deposito: number) {
+//         if (deposito <= 0) {
+//             throw `Operação de Depósito Inválida: R$ ${deposito}`
+//         }
+
+//         this.saldo += deposito
+//         return (`A conta número ${this.numeroConta}, recebeu um depósito de ${deposito} e tem de saldo R$ ${this.saldo}`)
+//     }
+//     verificarSaldo() {
+//         return (`A conta número ${this.numeroConta} tem de saldo R$ ${this.saldo}`)
+
+//     }
+//     sacar(saque: number) {
+//         if (saque <= 0 || saque > this.saldo) {
+//             throw `Operação de Saque Inválida: R$ ${saque}`
+//         }
+//         this.saldo -= saque
+//         return (`A conta número ${this.numeroConta}, realizou um saque de ${saque} e tem de saldo R$ ${this.saldo}`)
+
+//     }
+// }
+
+// var minhaConta = new ContaBancaria(123, 50000)
+// console.log(minhaConta.verificarSaldo())
+// console.log(minhaConta.depositar(250))
+// //console.log(minhaConta.depositar(0))   //com valor zero ele apresenta o throw e para de rodar o código.
+// console.log(minhaConta.sacar(45000))
+// console.log(minhaConta.sacar(6000))
+
+
+// 6. Crie uma classe Produto com os atributos nome, preco e quantidade e métodos para calcular o valor total (preço * quantidade).
+class Produto {
+    nome: string
+    preco: number
+    quantidade: number
+
+    constructor(nome: string, preco: number, quantidade: number) {
+        this.nome = nome
+        this.preco = preco
+        this.quantidade = quantidade
     }
 
-    depositar(deposito: number) {
-       if (deposito <=0){
-        throw `Operação de Depósito Inválida: R$ ${deposito}`
-       }
-        
-        this.saldo += deposito
-        return (`A conta número ${this.numeroConta}, recebeu um depósito de ${deposito} e tem de saldo R$ ${this.saldo}`)
+    valorTotal() {
+        return (`O produto ${this.nome} terá o valor final de ${(this.preco * this.quantidade)}`)
     }
-    verificarSaldo() {
-        return (`A conta número ${this.numeroConta} tem de saldo R$ ${this.saldo}`)
+}
+var valorProduto = new Produto("Sandália", 40, 5)
+console.log(valorProduto.valorTotal())
 
+// 7.	Crie uma classe Triangulo com os atributos lado1, lado2 e lado3 e um método para verificar se é um triângulo válido.
+class Triangulo {
+    lado1: number
+    lado2: number
+    lado3: number
+
+    constructor(lado1: number, lado2: number, lado3: number) {
+        this.lado1 = lado1
+        this.lado2 = lado2
+        this.lado3 = lado3
     }
-    sacar(saque: number) {
-        if (saque <= 0 || saque > this.saldo) {
-            throw `Operação de Saque Inválida: R$ ${saque}`
+
+    validade() {
+        if (this.isTrianguloValido(this.lado1, this.lado2, this.lado3)
+            || this.isTrianguloValido(this.lado2, this.lado1, this.lado3)
+            || this.isTrianguloValido(this.lado3, this.lado1, this.lado2)) {
+
+            return (`É um triângulo!`)
         }
-        this.saldo -= saque
-        return (`A conta número ${this.numeroConta}, realizou um saque de ${saque} e tem de saldo R$ ${this.saldo}`)
+        return (`Não é um triângulo!`)
+    }
 
+    isTrianguloValido(posicao1: number, posicao2: number, posicao3: number) {
+        if (posicao1 > (posicao2 - posicao3) && posicao1 < (posicao2 + posicao3)) {
+            return true
+        }
+        return false
+    }
+
+    validade_outra_opcao() {
+        if (this.lado1 > (this.lado2 - this.lado3) && this.lado1 < (this.lado2 + this.lado3)) {
+            return (`É um triângulo!`)
+        }
+        else if (this.lado2 > (this.lado1 - this.lado3) && this.lado2 < (this.lado1 + this.lado3)) {
+            return (`É um triângulo!`)
+        }
+        else if (this.lado3 > (this.lado1 - this.lado2) && this.lado3 < (this.lado1 + this.lado2)) {
+            return (`É um triângulo!`)
+        }
+        else
+            return (`Não é um triângulo!`)
+    }
+}
+var triangulo = new Triangulo(2, 2, 2)
+console.log(triangulo.validade())
+console.log(triangulo.validade_outra_opcao())
+
+// 8. Crie uma classe Livro com os atributos titulo, autor e anoPublicacao e um método para exibir informações do livro.
+class Livro {
+    titulo: string
+    autor: string
+    anoPublicacao: number
+
+    constructor(titulo: string, autor: string, anoPublicacao: number) {
+        this.titulo = titulo
+        this.autor = autor
+        this.anoPublicacao = anoPublicacao
+    }
+    informacaoLivro() {
+        return (`O livro é: ${this.titulo}. Autor: ${this.autor}. Ano de publicação: ${this.anoPublicacao}`)
+    }
+}
+var livro = new Livro("Nárnia", "C.S. Lewis", 2015)
+console.log(livro.informacaoLivro())
+
+// 9.	Crie uma classe Agenda que permite adicionar, listar e remover contatos (nomes e números de telefone).
+
+class Agenda {
+    listaContatos: Contato[] = new Array()
+
+    adicionarContato(contato: Contato) {
+        this.listaContatos.push(contato)
+    }
+
+    listarContatos() {
+        return this.listaContatos
+    }
+
+    removerContato(contato: Contato) {
+        for (var i = 0; i <= this.listaContatos.length; i++) {
+            if (contato.nome == this.listaContatos[i].nome && contato.numeroTelefone == this.listaContatos[i].numeroTelefone) {
+                this.listaContatos.splice(i)
+            }
+        }
     }
 }
 
-var minhaConta = new ContaBancaria(123, 50000)
-console.log(minhaConta.verificarSaldo())
-console.log(minhaConta.depositar(250))
-//console.log(minhaConta.depositar(0))   //com valor zero ele apresenta o throw e para de rodar o código.
-console.log(minhaConta.sacar(45000))
-console.log(minhaConta.sacar(6000))
+
+class Contato {
+    nome: string
+    numeroTelefone: number
+
+    constructor(nome: string, numeroTelefone: number) {
+        this.nome = nome
+        this.numeroTelefone = numeroTelefone
+    }
+}
+
+var agenda = new Agenda()
+var contato1 = new Contato("Ana", 12345)
+
+agenda.adicionarContato(new Contato("hsgfdh", 415687445))
+agenda.adicionarContato(contato1)
+console.log(agenda.listarContatos())
+agenda.removerContato(contato1)
+console.log(agenda.listarContatos())
+
+
+// 10.	Crie uma classe CarroEsportivo que herda da classe Carro (usada no exemplo anterior) e tenha métodos adicionais para acelerar rapidamente e produzir um som esportivo.
+class Carro {
+    modelo: string;
+    marca: string;
+    cor: string;
+    numeroRodas: number;
+
+    constructor(modelo: string, marca: string, cor: string, numeroRodas: number) {
+        this.modelo = modelo
+        this.marca = marca
+        this.cor = cor
+        this.numeroRodas = numeroRodas
+    }
+}
+class CarroEsportivo extends Carro {
+
+    constructor(modelo: string, marca: string, cor: string, numeroRodas: number) {
+        super(modelo, marca, cor, numeroRodas)
+    }
+
+    acelerar(){
+        return (`O carro acelerou`)
+    }
+
+    produzirSom(){
+        return (`Vrum...Vruum...`)
+    }
+}
+var carroEsportivo = new CarroEsportivo("Uno", "Fiat", "azul",4)
+console.log  (carroEsportivo.acelerar())
+console.log  (carroEsportivo.produzirSom())
