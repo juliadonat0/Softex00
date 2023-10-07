121a5521a893465c1f34a6d156e4e05834247221
Julia Donato <139878632+juliadonat0@users.noreply.github.com>
2023-10-07 18:18:31 +0000

    resolução completa

---
+++ padroes_de_desenvolvimento/padrao_prototype.ts
@@ -0,0 +1,16 @@
+class Veiculo {
+    modelo: string;
+    marca: string;
+    cor: string;
+    numeroRodas: number;
+
+
+    constructor (modelo: string, marca: string, cor: string, numeroRodas: number) {
+        this.modelo = modelo
+        this.marca = marca
+        this.cor = cor
+        this.numeroRodas = numeroRodas
+    } 
+
+
+}
