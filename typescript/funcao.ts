ece57c6eaeaa7d30480541f0a2a508b7662070bc
Julia Donato <139878632+juliadonat0@users.noreply.github.com>
2023-10-07 00:23:20 +0000

    resolução completa - exercício classe

--- typescript/funcao.ts
+++ typescript/funcao.ts
@@ -90,19 +90,20 @@ imprimirLista(["manga", "melão", "mamão"])
 console.log("exercício 6")
 function calcularMedia(numeros: Array<number>): number {
     var soma = 0
-    for (let contador = 0; contador < numeros.length; contador++){
+    for (let contador = 0; contador < numeros.length; contador++) {
         soma = soma + numeros[contador]
     }
-    return soma/numeros.length
+    return soma / numeros.length
 }
 
-console.log(calcularMedia([10,20,30]))
+console.log(calcularMedia([10, 20, 30]))
 
 
 // 7. Crie uma função chamada maiorNumero que receba um array de números como
 // parâmetro e exiba o maior número presente no array no console.
 console.log("exercício 7")
-function maiorNumero(numeros: Array<number>): number {
+
+function numeroMaior(numeros: Array<number>): number {
     let maior = 0
 
     for (let contador = 0; contador < numeros.length; contador++) {
@@ -112,22 +113,57 @@ function maiorNumero(numeros: Array<number>): number {
     }
     return maior
 }
-console.log(maiorNumero([100,250,300]))
+console.log(numeroMaior([100, 250, 300]))
 
 
 //8. Escreva uma função chamada contarVogais que aceite uma string como parâmetro e
 // conte quantas vogais (a, e, i, o, u) ela contém. Exiba o resultado no console.
 console.log("exercício 8")
-//function contarVogais():string{
 
+console.log(contadorVogais("socorro"))
+function contadorVogais(palavra: string): string {
 
-    
-//}
-
+    var arrayPalavra = palavra.split("")
+    var contadorVogais = 0
+    for (let contador = 0; contador < arrayPalavra.length; contador++) {
+        let letra = arrayPalavra[contador]
+        if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+            contadorVogais++
+        }
+    }
+    return "A quantidade de vogal é : " + contadorVogais
+}
 
 
 // 9. Crie uma função chamada imprimirTabuada que aceite um número como entrada e
 // exiba a tabuada desse número de 1 a 10 no console.
 
+console.log("exercício 9")
+function tabuada(numero: number): void {
+    for (let i = 1; i <= 10; i++) {
+        console.log(numero * i)
+    }
+}
+tabuada(5)
+
 //10.Escreva uma função chamada verificarPrimo que receba um número como parâmetro e
 // exiba no console se o número é primo ou não.
+
+console.log("exercício 10")
+var x = 211
+verificarPrimo(x)
+function verificarPrimo(x: number): void {
+    var cont = 1
+    var primo = 0
+    while (cont <= x) {
+        if (x % cont == 0) {
+            primo++
+        }
+        cont++
+    }
+    if (primo == 2) {
+        console.log(`O número ${x} é primo`)
+    } else {
+        console.log(`O número ${x} não é primo`)
+    }
+}
